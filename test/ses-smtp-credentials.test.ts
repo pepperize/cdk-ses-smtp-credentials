63a0f2814e5fbe50c2c3a18b0b4039446d1791e7
Patrick Florek <patrick.florek@gmail.com>
2021-12-11 14:51:46 +0100

    feat: add tests

---
+++ test/ses-smtp-credentials.test.ts
@@ -0,0 +1,35 @@
+import { Template } from "@aws-cdk/assertions";
+import { App, Aspects, Stack } from "@aws-cdk/core";
+import { AwsSolutionsChecks } from "cdk-nag";
+import { SesSmtpCredentials } from "../src";
+
+describe("SesSmtpCredentials", () => {
+  it("Should match snapshot", () => {
+    // Given
+    const app = new App();
+    const stack = new Stack(app);
+
+    // Then
+    new SesSmtpCredentials(stack, "SesSmtpCredentials", {
+      username: "Micha",
+    });
+    const template = Template.fromStack(stack);
+
+    // Then
+    expect(template).toMatchSnapshot();
+  });
+
+  it("Should comply to best practices", () => {
+    // Given
+    const app = new App();
+    const stack = new Stack(app);
+
+    // Then
+    new SesSmtpCredentials(stack, "SesSmtpCredentials", {
+      username: "Micha",
+    });
+
+    // Then
+    Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
+  });
+});
