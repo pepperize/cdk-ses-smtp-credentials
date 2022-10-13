d3cd5c9aaee87f98c3c2785e807988593e8f4ef3
Patrick Florek <patrick.florek@gmail.com>
2022-10-13 19:23:31 +0200

    chore: update snapshots

--- test/integ.default.test.ts
+++ test/integ.default.test.ts
@@ -1,16 +1,10 @@
-import { Aspects } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-import { AwsSolutionsChecks } from "cdk-nag";
-import { app, stack } from "../src/integ.default";
+import { stack } from "../src/integ.default";
 
 describe("integ.default", () => {
   it("Should match snapshot", () => {
     // When
     const t = Template.fromStack(stack);
     expect(t).toMatchSnapshot();
-  });
-  it("Should comply to best practices", () => {
-    // When
-    Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
   });
 });
